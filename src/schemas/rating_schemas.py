83e79b6ad7e5593f0c113d32842dd17fa7707d5b
tetianakondra <tatjana.ukon@gmail.com>
2023-06-11 00:37:10 +0300

    add ratings to models, repository, routes, schemas + route to ratings_main

---
+++ src/schemas/rating_schemas.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from typing import Dict
+
+from user_schemas import UserResponse
+
+class ViewRatingModel(BaseModel):
+    one_star: bool
+    two_stars: bool
+    three_stars: bool
+    four_stars: bool
+    five_stars: bool
+
+class RatingModel(BaseModel):
+    rating: Dict
+    user_id: int = Field(1, gt=0)
+    image_id: int = Field(1, gt=0)
+
+
+class RatingResponse(BaseModel):
+    id: int = 1
+    rating: dict = {"one_star": False, "two_stars": False, "three_stars": False, "four_srats": False, "five_stars": False}
+    user: UserResponse
+
+    class Config:
+        orm_mode = True
+
